4019afe38cad74d16cdddd343c1b366456b84f52
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-01-04 21:58:11 +0900

    Add comment

--- app/frontend/src/js/application.js
+++ app/frontend/src/js/application.js
@@ -6,9 +6,17 @@
 //     document.addEventListener('DOMContentLoaded', () => {
 //       const app = new Vue({
 //         render: h => h(App)
+//         // hとは？ https://github.com/vuejs-templates/webpack-simple/issues/29
 //       }).$mount()
+//        // vm.$mount() は アンマウントな Vue インスタンスのマウンティングを手動で開始するために使用することができます。
+//        // https://jp.vuejs.org/v2/api/index.html
+//        // インスタンスを生成し、#app にマウント(#app を置換)
+//        //new MyComponent().$mount('#app')
+//        // 上記と同じです:
+//        //new MyComponent({ el: '#app' })
 //       document.body.appendChild(app.$el)
-
+//       // body の末に app.$el を追加している
+//       // $el は Vue インスタンス
 //       console.log(app)
 //     })
 //   }
